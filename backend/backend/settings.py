0b93a7f8009df1e75cc0d41bcfabedfccb45fb11
banushasarma <sharmabanu03@gmail.com>
2026-01-22 19:18:46 +0530

    Migrate to POSGRE

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobportal_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432', 
     }
 }
 
