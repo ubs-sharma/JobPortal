83d1f38a562f2fef554ba404e20a83374e1ae8e2
banushasarma <sharmabanu03@gmail.com>
2026-01-22 20:56:07 +0530

    Login and Register added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from .views import hello_api, register_user        
+from .views import hello_api, register_user, basic_login
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("hello/",  hello_api),
-    path("register/", register_user)
+    path("hello/", hello_api),
+    path("register/", register_user),
+    path("login/", basic_login),
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -3,16 +3,35 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
 
+from django.contrib.auth.models import User  # django.contrib.auth.models
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def hello_api(request):
-    return Response({"message": "hello from Django API","status": "ok"})
+    return Response({"message": "hello from Django API", "status": "ok"})
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def register_user(request):
-    serializer = RegisterSerializer(data = request.data)
+    serializer = RegisterSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response({"message": "user registered","status": "ok"}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"message": "user registered", "status": "ok"},
+            status=status.HTTP_201_CREATED,
+        )
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(["POST"])
+def basic_login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    # Authentication logic here
+    try:
+        user = User.objects.get(username=username, password=password)
+        return Response({"message": "Login success..!!"}, status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response(
+            {"message": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+        )
