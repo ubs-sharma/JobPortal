7f6a2404c1d4ea9bcc847edb746bc19511fd82a0
banushasarma <sharmabanu03@gmail.com>
2026-01-23 20:06:15 +0530

    Login front end added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { LoginPage } from "./LoginPage";
 import { RegisterPage } from "./RegisterPage";
 
 function App() {
   return (
     <>
-      <RegisterPage />
+      <LoginPage />
     </>
   );
 }
