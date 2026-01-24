3f7110a0e6d77c357ff90ffeb616f97a8fc50fd7
banushasarma <sharmabanu03@gmail.com>
2026-01-24 14:37:36 +0530

    Router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,20 @@ import { ApplyJobPage } from "./ApplyJobPage";
 import { JobListingPage } from "./JobListingPage";
 import { LoginPage } from "./LoginPage";
 import { RegisterPage } from "./RegisterPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <ApplyJobPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/jobs" element={<JobListingPage />} />
+          <Route path="/apply/:jobId" element={<ApplyJobPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
